a16b4ce195a55ece7680e735a8fa649cd1ca76e9
Andrei Khegai <hegai.andrei@gmail.com>
2021-07-23 14:22:04 +0600

    Initial Commit

---
+++ Shared/tomiApp.swift
@@ -0,0 +1,17 @@
+//
+//  tomiApp.swift
+//  Shared
+//
+//  Created by Andrei Khegai on 23/7/21.
+//
+
+import SwiftUI
+
+@main
+struct tomiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
